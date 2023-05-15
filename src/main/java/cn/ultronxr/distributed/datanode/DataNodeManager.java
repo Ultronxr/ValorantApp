049840e086acea4df5fc939fecde78c6ef30a2fe
ultronxr <Ultronxr@users.noreply.github.com>
2023-05-15 19:27:08 +0800

    完成 redis topic channel 发布与订阅功能，该 channel 用于主数据节点发布数据更新任务、子数据节点订阅数据更新任务。 详细流程：管理员在网页（部署在主数据节点上）点击更新按钮，主数据节点自己开始更新数据的同时向 topic channel 发布更新任务，子数据节点订阅获取到更新任务，也跟着开始更新数据

--- src/main/java/cn/ultronxr/distributed/datanode/DataNodeManager.java
+++ src/main/java/cn/ultronxr/distributed/datanode/DataNodeManager.java
@@ -1,6 +1,9 @@
 package cn.ultronxr.distributed.datanode;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -23,11 +26,18 @@ import javax.annotation.PostConstruct;
 public class DataNodeManager implements ApplicationRunner {
 
     @Autowired
+    private RedissonClient redissonClient;
+
+    @Autowired
     private RedisTemplate<Object, Object> redisTemplate;
 
-    /** redis zset key */
-    @Value("${valorant.datanode.redis-key}")
-    private String REDIS_KEY;
+    /** zset 的 redis key ，该 zset 用于维护所有在线的数据节点ID */
+    @Value("${valorant.datanode.redis-zset-key}")
+    private String redisZsetKey;
+
+    /** redis topic 的名称，该 topic 用于主数据节点发布数据更新任务、子数据节点订阅数据更新任务 */
+    @Value("${valorant.datanode.redis-topic-channel}")
+    private String redisTopicChannel;
 
     /** 主数据节点ID */
     @Value("${valorant.datanode.main-datanode-id}")
@@ -47,12 +57,12 @@ public class DataNodeManager implements ApplicationRunner {
     }
 
     /**
-     * 清空 datanode （早上6点由 main datanode 执行）
+     * 清空 datanode redis zset （定时由 main datanode 执行）
      */
     @Scheduled(cron = "${valorant.datanode.cron.clear-datanode}")
     public void clearDataNode() {
-        if(dataNodeId.equals(mainDataNodeId)) {
-            Boolean res = redisTemplate.delete(REDIS_KEY);
+        if(isMainDataNode()) {
+            Boolean res = redisTemplate.delete(redisZsetKey);
             if(null != res && res) {
                 log.info("清空 DataNode ZSet 完成。");
             }
@@ -60,30 +70,40 @@ public class DataNodeManager implements ApplicationRunner {
     }
 
     /**
-     * 插入 nodeId （早上7点统一写入ID）
+     * 向 redis zset 中插入 nodeId （默认定时统一写入ID，或者由数据节点主动调用）
      */
     @Scheduled(cron = "${valorant.datanode.cron.add-datanode}")
     public void addDataNode() {
-        Boolean res = redisTemplate.opsForZSet().add(REDIS_KEY, dataNodeId, 1);
+        Boolean res = redisTemplate.opsForZSet().add(redisZsetKey, dataNodeId, 1);
         if(null != res && res) {
             log.info("向 ZSet 中添加 dataNodeId={}", dataNodeId);
         }
     }
 
     /**
-     * 获取总节点数量
+     * 从 redis zset 中获取总节点数量
      */
     public Long getTotal() {
-        return redisTemplate.opsForZSet().size(REDIS_KEY);
+        return redisTemplate.opsForZSet().size(redisZsetKey);
     }
 
     /**
-     * 获取当前节点下标/排名（下标从0开始）
+     * 从 redis zset 中获取当前数据节点下标/排名（下标从0开始）
      */
     public Long getIndex() {
-        return redisTemplate.opsForZSet().rank(REDIS_KEY, dataNodeId);
+        return redisTemplate.opsForZSet().rank(redisZsetKey, dataNodeId);
     }
 
+    /**
+     * 检查本数据节点自身是否是主数据节点
+     */
+    public boolean isMainDataNode() {
+        return mainDataNodeId.equals(getDataNodeId());
+    }
+
+    /**
+     * 获取本数据节点的ID
+     */
     public String getDataNodeId() {
         return dataNodeId;
     }
@@ -93,6 +113,22 @@ public class DataNodeManager implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         addDataNode();
         log.info("项目启动时自动注册 datanode 完成。");
+    }
+
+    public RTopic getRTopic() {
+        // 请注意：这里的 codec 需要与 topic channel 中发送的消息类型对应，否则会报无法解码数据异常！
+        return redissonClient.getTopic(redisTopicChannel, new StringCodec());
+    }
+
+    /**
+     * 主数据节点向 redis topic channel 中发布更新任务（所有子数据节点订阅获取到这个任务之后，进行数据更新）（1对多）<br/>
+     * 有关子数据节点的订阅，请查看 {@link cn.ultronxr.valorant.controller.StoreFrontController#run(ApplicationArguments)}
+     */
+    public void publishUpdateMission() {
+        String missionName = "batchUpdateBoth";
+        RTopic rtopic = getRTopic();
+        rtopic.publish(missionName);
+        log.info("主数据节点向 redis topic channel 发布更新任务，channel={}, msg={}", rtopic.getChannelNames(), missionName);
     }
 
 }
--- src/main/java/cn/ultronxr/valorant/controller/StoreFrontController.java
+++ src/main/java/cn/ultronxr/valorant/controller/StoreFrontController.java
@@ -2,12 +2,19 @@ package cn.ultronxr.valorant.controller;
 
 import cn.ultronxr.common.bean.AjaxResponse;
 import cn.ultronxr.common.util.AjaxResponseUtils;
+import cn.ultronxr.distributed.datanode.DataNodeManager;
 import cn.ultronxr.framework.annotation.AdminAuthRequired;
 import cn.ultronxr.valorant.bean.DTO.BatchQueryBothDTO;
 import cn.ultronxr.valorant.service.StoreFrontService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.redisson.api.RTopic;
+import org.redisson.api.listener.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +26,13 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/valorant/storefront")
 @Slf4j
-public class StoreFrontController {
+public class StoreFrontController implements ApplicationRunner {
 
     @Autowired
     private StoreFrontService sfService;
+
+    @Autowired
+    private DataNodeManager dataNodeManager;
 
 
     //@GetMapping("/singleItemOffers")
@@ -47,10 +57,37 @@ public class StoreFrontController {
     @PostMapping("/batchUpdateBoth")
     @ResponseBody
     public AjaxResponse batchUpdateBoth() {
+        // 如果是主数据节点，那么同时向 redis topic 发布更新任务，触发子数据节点同时开始更新
+        if(dataNodeManager.isMainDataNode()) {
+            dataNodeManager.publishUpdateMission();
+        }
+        // 数据节点本身开始数据更新
         if(sfService.batchUpdateBoth(true)) {
             return AjaxResponseUtils.success();
         }
         return AjaxResponseUtils.fail();
+    }
+
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if(dataNodeManager.isMainDataNode()) {
+            return;
+        }
+        String missionName = "batchUpdateBoth";
+        // 所有子数据节点订阅 redis topic channel ，如果收到主数据节点发布的更新任务，那么进行数据更新
+        RTopic rtopic = dataNodeManager.getRTopic();
+        rtopic.addListener(String.class, new MessageListener<String>() {
+            @Override
+            public void onMessage(CharSequence channel, String msg) {
+                log.info("接收到主数据节点发布的任务，channel={}, msg={}", channel, msg);
+                if(StringUtils.isNotEmpty(msg) && msg.equals(missionName)) {
+                    log.info("子数据节点开始更新数据。");
+                    sfService.batchUpdateBoth(true);
+                }
+            }
+        });
+        log.info("子数据节点订阅 redis topic channel - {} 完成。", rtopic.getChannelNames());
     }
 
     @PostMapping("/batchQueryBoth")
